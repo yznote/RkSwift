991c5310ca708d2bea228408dab845e0dc73cef9
yuan <yzyuan920@163.com>
2024-12-25 10:51:10 +0800

    test-format

--- RKProject/Test/SwiftyJsonVC.swift
+++ RKProject/Test/SwiftyJsonVC.swift
@@ -1,10 +1,3 @@
-//
-//  SwiftyJsonVC.swift
-//  RKProject
-//
-//  Created by yunbao02 on 2024/12/21.
-//
-
 import FlexLayout
 import PinLayout
 import SwifterSwift
@@ -14,36 +7,36 @@ import UIKit
 class SwiftyJsonVC: RKBaseVC {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         jsonTest()
         // pinTest()
         // swifterTest()
     }
-    
+
     /// swifterswift
     func swifterTest() {
         // let str1:String = ""
         // let str2:String = "   "
-        
+
         // dic
         // let dic: [String:Any] = ["id":1,"name":"小明"]
         // let res = dic.has(key: "id")
         // print("===res:\(res)")
-        
+
         //
         // let lab = UILabel()
         // lab.font = lab.font.italic;
-        
+
         // var tab = UITableView()
         // tab.register(cellWithClass: RKBaseTableCell.self)
-        
+
         // 时间戳
         // let timestamp = Date().unixTimestamp
         // print("===>time1:\(timestamp)")
         // let intTime = Int(timestamp*1000)
         // print("===>time2:\(intTime)")
     }
-    
+
     /// swiftyjson
     func jsonTest() {
         let dic: [String: Any] = [
@@ -51,14 +44,14 @@ class SwiftyJsonVC: RKBaseVC {
             "uid": "123",
         ]
         let newDic = JSON(dic)
-        
+
         let action = newDic["action"].stringValue
         if action == "1" {
             print("enter-:\(action)")
         } else {
             print("enter-else:\(action)")
         }
-        
+
         // 判空
         let oeStr = newDic["uid"].stringValue
         if oeStr.isEmpty {
@@ -66,7 +59,7 @@ class SwiftyJsonVC: RKBaseVC {
         } else {
             rkprint("===>不为空\(JSON(newDic))")
         }
-        
+
         let array: [Any] = [
             ["uid": 1234, "name": "na", "age": 13],
             ["uid": 123, "name": "na", "age": 13],
@@ -75,14 +68,15 @@ class SwiftyJsonVC: RKBaseVC {
         rkprint("====>", array)
         rkprint("====>", JSON(array))
         debug.log("debug:", newDic)
-        
+
         let idx = array.firstIndex(
             // where: {($0 as! [String:Any])["uid"] as! Int == 123
             where: { JSON($0)["uid"].intValue == 1235
-            })
-        // debug.log("array-idx:\(idx ?? -10)")
+            }
+        )
+        debug.log("array-idx:\(idx ?? -10)")
     }
-    
+
     /// pin
     func pinTest() {
         let btn = UIButton(type: .custom)
@@ -90,22 +84,22 @@ class SwiftyJsonVC: RKBaseVC {
         btn.addTarget(self, action: #selector(clickBtn), for: .touchUpInside)
         view.addSubview(btn)
         btn.pin.width(60).height(20).bottom(10%).hCenter()
-        
+
         print("===>running")
-        
+
         let btn2 = UIButton()
         btn2.backgroundColor = .hex("000000")
         view.addSubview(btn2)
         // btn2.pin.width(of: btn).height(of: btn).left(of: btn, aligned: .center) // 在左边
         // btn2.pin.width(of: btn).height(of: btn).before(of: btn,aligned: .center) // 左
-        
+
         // btn2.pin.width(of: btn).height(of: btn).right(of: btn,aligned: .center) // 在右边
         // btn2.pin.width(of: btn).height(of: btn).after(of: btn,aligned: .center)
-        
+
         // btn2.pin.width(of: btn).height(of: btn).below(of: btn, aligned: .center) // 在下面
         btn2.pin.width(of: btn).height(of: btn).above(of: btn, aligned: .center) // 上
     }
-    
+
     @objc func clickBtn() {
         print("===>log")
     }
